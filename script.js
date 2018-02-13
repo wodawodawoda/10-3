a84ad8c8d01dd5f8829792bae20d733ac728d64c
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-02-13 17:24:14 +0100

    Add counter controls

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+var carouselList = $('#carousel ul');
+var	backward = $('#backward');
+var	forward = $('#forward');
+var counter = 0;
+var	control = document.getElementsByClassName('control-item');
 $(function() {
-	var carouselList = $('#carousel ul');
 
 	function moveFirstSlide () {
 		var firstItem = carouselList.find('li:first'),
 			lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
+		counter++;
+		console.log(counter);
+		if (counter % control.length !== 0) {
+			control[counter % control.length - 1].style.background = '#FFF'
+		} else {
+			control[control.length - 1].style.background = '#FFF'
+		}
+		control[counter % control.length].style.background = '#00F'
+
 	}
 
 	function changeSlide() {
 		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 	}
 
-	setInterval(changeSlide, 3000);	
+	setInterval(changeSlide, 1000);
+
 });

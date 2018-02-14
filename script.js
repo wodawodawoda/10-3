c29590762e18e314ed0f4c9089fd44a9c006c20c
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-02-14 23:34:03 +0100

    Add buttons

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
-var carouselList = $('#carousel ul');
-var	backward = $('#backward');
-var	forward = $('#forward');
-var counter = 0;
-var	control = document.getElementsByClassName('control-item');
 $(function() {
+	var carouselList = $('#carousel ul');
+	var backward = $('#backward');
+	var forward = $('#forward');
+	var counter = 0;
+	var control = document.getElementsByClassName('control-item');
 
-	function moveFirstSlide () {
+	control[counter].style.background = '#00F';
+
+	function moveFirstSlide() {
 		var firstItem = carouselList.find('li:first'),
 			lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
+		carouselList.css({ marginLeft: 0 });
 		counter++;
-		console.log(counter);
-		if (counter % control.length !== 0) {
-			control[counter % control.length - 1].style.background = '#FFF'
-		} else {
-			control[control.length - 1].style.background = '#FFF'
+		if (counter == 5) {
+			counter = 0;
 		}
-		control[counter % control.length].style.background = '#00F'
+		control[counter].style.background = '#00F';
+		if (counter == 0) {
+			control[control.length - 1].style.background = "#FFF";
+		} else {
+			control[counter - 1].style.background = "#FFF";
+		}
+		
+	}
 
+	function moveBack() {
+		var firstItem = carouselList.find('li:first'),
+			lastItem = carouselList.find('li:last');
+		firstItem.before(lastItem);
+		carouselList.css({ marginLeft: -400 });
+		counter--;
+		if (counter < 0) {
+			counter = 4;
+		}
+		control[counter].style.background = '#00F';
+		if (counter == control.length - 1) {
+			control[0].style.background = "#FFF";
+		} else {
+			control[counter + 1].style.background = "#FFF";
+		}
 	}
 
 	function changeSlide() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+		carouselList.animate({ 'marginLeft': -400 }, 500, moveFirstSlide);
 	}
 
-	setInterval(changeSlide, 1000);
+	forward.click(changeSlide);
+	backward.click(function() {
+		moveBack();
+		carouselList.animate({ 'marginLeft': 0 }, 500);
+	});
+	// setInterval(changeSlide, 2000);
+	function controls() {
+		var controlling = $('.control-item');
+	}
 
 });
